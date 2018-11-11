a7a99ac1b54a65f9f5e0b1d578a2154cdad8922e
Karl Koscher <supersat@cs.washington.edu>
2018-11-10 22:34:43 -0800

    Update javascript to use/generate a uid

--- netflix_tension/background.js
+++ netflix_tension/background.js
@@ -1,16 +1,13 @@
-var user = 'cam'; 
-
+var uid;
 var metadata;
-
 var time = {}; 
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+function handleMessage(request, sender, sendResponse) {
 	if (request.type == 'getMetadata'){
 	    sendResponse({metadata:metadata});
 	}
 	if (request.type == 'getUser'){
-	    sendResponse({user:user});
+	    sendResponse({user:uid});
 	}
 	if (request.type == 'update metadata'){
 	    metadata = request.metadata;
@@ -23,18 +20,15 @@ chrome.runtime.onMessage.addListener(
 
 	    // The URL to POST our data to
 	    var postUrl = 'https://homes.cs.washington.edu/~cobbc12/submit_tv_data.php';
-
 	    
 	    var xhr = new XMLHttpRequest();
 	    xhr.open('POST', postUrl, true);
-
-	    
 
 	    // Prepare the data to be POSTed by URLEncoding each field's contents
 	    var url = sender.tab.url;
 	    var sendertime = request.videoTime;
 	    
-	    var params = 'user=' + encodeURIComponent(user) + 'url=' + encodeURIComponent(url) +
+	    var params = 'uid=' + encodeURIComponent(uid) + 'url=' + encodeURIComponent(url) +
 		'&videotime=' + encodeURIComponent(sendertime) ;
 
 	    // Replace any instances of the URLEncoded space char with +
@@ -43,16 +37,37 @@ chrome.runtime.onMessage.addListener(
 	    // Set correct header for form data
 	    var formContentType = 'application/x-www-form-urlencoded';
 	    xhr.setRequestHeader('Content-type', formContentType);
-
 	    
-	    xhr.send(params);
-	    
+	    xhr.send(params);	    
 	}      
-      
-  });
+}
+
+function onConsentObtained(_uid) {
+	if (uid) {
+		return;
+	}
+	uid = _uid;
+	chrome.runtime.onMessage.addListener(handleMessage);
+}
 
 chrome.runtime.onInstalled.addListener(function (object) {
 	if (object.reason == chrome.runtime.OnInstalledReason.INSTALL) {
-		chrome.tabs.create({url:chrome.extension.getURL("consent.html")});
+		chrome.storage.local.get('uid', function(items) {
+			if (!('uid' in items)) {
+				chrome.tabs.create({url:chrome.extension.getURL("consent.html")});
+			}
+		});
+	}
+});
+
+chrome.storage.local.get('uid', function(items) {
+	if ('uid' in items) {
+		onConsentObtained(items.uid);
+	} else {
+		chrome.storage.onChanged.addListener(function(changes, namespamce) {
+			if ('uid' in changes) {
+				onConsentObtained(changes.uid.newValue);
+			}
+		});
 	}
 });
